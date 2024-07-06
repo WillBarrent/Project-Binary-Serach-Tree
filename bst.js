96b4c15a42bf2fec63c6371438629cd592ba6ad7
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-07-06 15:05:13 +0500

    Project Initialization

---
+++ bst.js
@@ -0,0 +1,35 @@
+import { Node } from "./node.js";
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    buildTree(array) {
+        
+    }
+
+    insert(value) {}
+    
+    deleteItem(value) {}
+
+    find(value) {}
+
+    levelOrder(callback) {}
+
+    inOrder(callback) {}
+
+    preOrder(callback) {}
+
+    postOrder(callback) {}
+
+    height(node) {}
+
+    depth(node) {}
+
+    isBalanced() {}
+
+    rebalance() {}
+}
+
+export { Tree }
