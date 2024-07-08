4aca75112e575d45dd091b1239bc2f028872b1ad
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2024-07-08 16:42:57 +0500

    Implement buildTree, Insert, Delete, Find Methods

--- bst.js
+++ bst.js
@@ -3,33 +3,106 @@ import { Node } from "./node.js";
 class Tree {
     constructor() {
         this.root = null;
+        this.queue = [];
     }
 
-    buildTree(array) {
+    buildTree(array, start = 0, end = array.length - 1) {
+        if (start > end) {
+            return null;
+        }
+
+        const middle = Math.floor((start + end) / 2);
+
+        const node = new Node(array[middle]);
+
+        node.left = this.buildTree(array, start, middle - 1);
+        node.right = this.buildTree(array, middle + 1, end);
+    
+        return node;
+    }
+
+    insert(value, node = this.root) {
+        if (node === null) {
+            node = new Node(value);
+            return node;
+        }
+
+        if (node.data > value) {
+            node.left = this.insert(value, node.left);
+        } else {
+            node.right = this.insert(value, node.right);
+        }
+
+        return node;
+    }
+
+    deleteItem(value, node = this.root) {
+        if (node.left === null && node.right === null) {
+            node = null;
+            return node;
+        }
+
+        if (node.data > value) {
+            node.left = this.deleteItem(value, node.left);
+        } else if (node.data < value) {
+            node.right = this.deleteItem(value, node.right);
+        } else {
+            if (node.left === null) {
+                return node.right;
+            } else if (node.right === null) {
+                return node.left;
+            }
+
+            node.data = this.minValue(node.right);
+            node.right = this.deleteItem(node.data, node.right);
+        }
+
+        return node;
+    }
+
+    minValue(node) {
+        let minVal = node.data;
+        while (node.left !== null) {
+            minVal = node.left.data;
+            node = node.left;
+        }
+
+        return minVal;
+    }
+
+    find(value, node = this.root) {
+        if (node.data === value) {
+            return node;
+        }
+
+        let returnNode = null;
+
+        if (node.data > value) {
+            returnNode = this.find(value, node.left);
+        } else {
+            returnNode = this.find(value, node.right);
+        }
+
+        return returnNode;
+    }
+
+    levelOrder(callback) {
         
     }
 
-    insert(value) {}
-    
-    deleteItem(value) {}
+    inOrder(callback) { }
 
-    find(value) {}
+    preOrder(callback) { }
 
-    levelOrder(callback) {}
+    postOrder(callback) { }
 
-    inOrder(callback) {}
+    height(node) { }
 
-    preOrder(callback) {}
+    depth(node) { }
 
-    postOrder(callback) {}
+    isBalanced() { }
 
-    height(node) {}
-
-    depth(node) {}
-
-    isBalanced() {}
-
-    rebalance() {}
+    rebalance() { }
 }
 
 export { Tree }
---
+++ index.js
@@ -0,0 +1,38 @@
+import { Tree } from "./bst.js";
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+        return null;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "|   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "|   "}`, true);
+    }
+}
+
+const sortAndRemove = (array) => {
+    array.sort((a, b) => a - b);
+
+    const newArray = [];
+
+    for (let i = 0; i < array.length;) {
+        newArray.push(array[i]);
+        const similarValues = array.filter(val => val === array[i]);
+        i += similarValues.length;
+    }
+
+    return newArray;
+}
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+const tree = new Tree();
+
+tree.root = tree.buildTree(sortAndRemove(array));
+
+prettyPrint(tree.root);
+
+console.log(tree.find(4));
